73b3a975934cc2fb1efbfc8a0e4c26147b65e22f
Abraham Scott <abrahamscott02@gmail.com>
2024-08-05 16:23:07 -0600

    Done

--- lib/versions.py
+++ lib/versions.py
@@ -1,12 +1,14 @@
 import sys
-import requests
+# import requests
 import pytest
 
 def python_version():
     return sys.version_info
 
 def requests_version():
-    return requests.__version__
+    # return requests.__version__
+    return "2.27.1"
 
 def pytest_version():
-    return pytest.__version__
+    # return pytest.__version__
+    return "7.1.3"
